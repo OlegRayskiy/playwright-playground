9db0a642350b18c32ecd55d870e2ddacc75633f8
Oleg Rayskiy <oleg.rayskiy@causeway.com>
2024-04-10 20:06:08 +0100

    c2

--- tests/example2.ts
+++ tests/example2.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 export default function createTests() {
-  test("homepage has title and links to intro page", async ({ page }) => {
+  test("homepage has title and links to intro page 2", async ({ page }) => {
     await page.goto("https://playwright.dev/");
 
     // Expect a title "to contain" a substring.
